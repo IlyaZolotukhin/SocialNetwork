d05654b54c217ec63696711200b57101339ba066
IlyaZolotuhin <1zolotyhin1@gmail.com>
2023-05-04 14:57:55 +0700

    change components

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+const Navbar = () => {
+    return (
+        <div>
+           <ul>
+               <li>Home</li>
+               <li>News</li>
+               <li>Messages</li>
+               <li>4</li>
+           </ul>
+        </div>
+    );
+}
+
+export default Navbar;
